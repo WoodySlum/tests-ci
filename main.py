0ef93234bc73c21ce912218820e57c570f3f54ba
smizrahi <seb.mizrahi@gmail.com>
2025-03-16 11:44:19 +0100

    Fix Black formatting issues

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 Main module for the Hello World program.
 """
 
+
 def hello():
     """Returns a simple greeting."""
     return "Hello, world!"
+
 
 if __name__ == "__main__":
     print(hello())

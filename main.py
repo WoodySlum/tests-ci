239f5e2cd753e9e36a18d6ffa82a845493d427e8
smizrahi <seb.mizrahi@gmail.com>
2025-03-16 11:43:27 +0100

    Fix pylint

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def hello():
-    return "Hello, world!"
+"""
+Main module for the Hello World program.
+"""
 
+def hello():
+    """Returns a simple greeting."""
+    return "Hello, world!"
 
 if __name__ == "__main__":
     print(hello())
